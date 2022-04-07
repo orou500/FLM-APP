8f570fa5ec13f068526b3d6efe994da2e00088ee
Or <orou500@gmail.com>
2022-04-07 15:39:06 +0300

    User timestamp

--- models/User.js
+++ models/User.js
@@ -25,7 +25,11 @@ const userSchema = new mongoose.Schema({
         required: [true, 'Please enter your last name'],
         minlength: [2, 'last name must be at least 2 charcters'],
     }
-})
+},
+{
+    timestamps: true,
+}
+)
 
 //fire a fumction before doc saved to db
 userSchema.pre('save', async function (next) {
