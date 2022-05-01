a14956ab18412cdef55c685f7e5a14d15936dda0
orou500 <87994914+orou500@users.noreply.github.com>
2022-05-01 03:25:49 +0300

    real time updates on matches changes by socket.io

--- server.js
+++ server.js
@@ -3,5 +3,29 @@ const app = require('./app')
 const port = process.env.PORT || 5000
 
 const server = http.createServer(app);
+io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+      });
+
+      socket.on('newMatch', (title) => {
+        io.emit('newMatch');
+      });
+
+      socket.on('deletedMatch', () => {
+        io.emit('newMatch');
+      });
+      socket.on('updateMatch', () => {
+        io.emit('newMatch');
+      });
+
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+});
 
 server.listen(port)
